321331d4af7ec8b87aefd9c9b9dbb708ee2f5ec0
Sangjun Lee <lsjmc0224@dgu.edu>
2023-06-02 16:45:35 +0900

    Fix: url 경로 앱별로 정리

--- Lumiticket/Lumiticket/urls.py
+++ Lumiticket/Lumiticket/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from mainpage import views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.intropage, name="intropage"),
-    path('loadingpage/', views.loadingpage, name="loadingpage"),
+
+    path('', include('mainpage.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
