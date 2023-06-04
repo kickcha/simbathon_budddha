654ea65d206e4a7663c00d3c07509d76eaea9020
lsjmc0224 <lsjmc0224@dgu.edu>
2023-06-04 09:43:23 +0900

    FEAT:login, signup구현

--- Lumiticket/accounts/urls.py
+++ Lumiticket/accounts/urls.py
@@ -4,4 +4,6 @@ from .views import * # accounts/views에서 모든 함수를 가져온다.
 app_name = "accounts"
 urlpatterns = [
     path('login/', login, name="login"),
+    path('logout/', logout, name="logout"),
+    path('signup/', signup, name="signup"),
 ]
--- Lumiticket/accounts/views.py
+++ Lumiticket/accounts/views.py
@@ -1,11 +1,52 @@
 from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth.models import User
+from .models import Profile # 모델 사용
 
 # Create your views here.
 def login(request):
     if request.method == 'POST':
-        pass
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authentication(request, username = username, password = password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('mainpage.mainpage')
+        else:
+            return render(request, 'accounts/login.html') # '정보 없음, 회원가입 물어보는 페이지' 가기로 바꾸기
+        
     elif request.method == 'GET':
         return render(request, 'accounts/login.html')
 
-# acccounts/login -> 구글
-# login -> 원하는 페이지로 안갈수도 있다. 
+def logout(request):
+    auth.logout(request)
+    return redirect('mainpage:mainpage')
+
+def signup(request):
+    if request.method == 'POST':
+        # 변수 받아오기
+        username = request.POST['username']
+        nickname = request.POST['nickname']
+        password = request.POST['password']
+        confirm = request.POST['confirm']
+
+        if not username or not password:
+            error_username = '아이디와 비밀번호를 입력해주세요.'
+            return render(request, 'accounts/signup.html', {'error_username': error_username})
+        if not nickname:
+            error_nickname = '닉네임을 입력해주세요.'
+            return render(request, 'accounts/signup.html', {'error_nickname': error_nickname})
+        if password != confirm:
+            error_password = '비밀번호와 비밀번호 확인이 일치하지 않습니다.'
+            return render(request, 'accounts/signup.html', {'error_password': error_password})
+        else:
+            user = User.objects.create_user(username=username, password=password)
+
+            profile = Profile(user=user, nickname=nickname)
+            profile.save()
+
+            auth.login(request,user)
+            return redirect('mainpage')
+        
+    return render(request, 'accounts/signup.html') # 
