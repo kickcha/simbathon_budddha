1d888b6d4fc376a403800ee8f1e39ae7deec28be
lsjmc0224 <lsjmc0224@dgu.edu>
2023-06-21 01:29:00 +0900

    BE_Qna model 추가

--- Lumiticket/qnapage/models.py
+++ Lumiticket/qnapage/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Qna(models.Model):
+    title = models.CharField(max_length = 200)
+    writer = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
+    body = models.TextField()
+    pub_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
